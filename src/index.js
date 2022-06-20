fd5cdc11ff9ad04942b7ffd03d9be7daa0311b1e
Amalidi <A.idi12@outlook.com>
2022-06-20 17:15:49 +0100

    added api for the routers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const routes = require("./routes");
+const router = require("./routes");
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.use(routes);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
