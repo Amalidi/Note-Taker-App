18c17ebe536a5d13f899983c12ee42c87738b71c
Amalidi <A.idi12@outlook.com>
2022-06-21 17:38:54 +0100

    set up the notes for controllers

---
+++ src/controllers/notes.js
@@ -0,0 +1,55 @@
+//require utils and other modules
+const {
+  readFromDataFile,
+  writeToDataFile,
+} = require("../utils/interactWithDF");
+
+const { v4: uuidv4 } = require("uuid");
+
+const getNotes = (req, res) => {
+  // read from data file
+  const notes = readFromDataFile("db");
+  // send data and return response
+  return res.json(notes);
+};
+
+function deleteNote(req, res) {
+  // get id for the note
+  const { id } = req.params;
+
+  // get all data from file
+  const notes = readFromDataFile("db");
+
+  // remove note from file
+  const filteredDataFile = notes.filter((item) => {
+    item.id !== id;
+  });
+
+  // write into data file
+  writeToDataFile("db", filteredDataFile);
+
+  // send response
+  res.json({
+    message: "Note has been successfully delated!",
+  });
+}
+
+const createNote = (req, res) => {
+  //get data from db
+  const notes = readFromDataFile("db");
+
+  //get data from the user
+  const { title, text } = req.body;
+
+  //generate unique id
+  const id = uuidv4();
+
+  //write note into data file
+  const newNote = { id, title, text };
+  writeToDataFile("db", [newNote, ...notes]);
+
+  //send file as response - is it needed?
+  res.json(newNote);
+};
+
+module.exports = { getNotes, deleteNote, createNote };
