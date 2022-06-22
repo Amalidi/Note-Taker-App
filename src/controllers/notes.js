eda67e4fbbe6853ee7a0348f4cbb5f161c063004
Amalidi <A.idi12@outlook.com>
2022-06-22 03:17:57 +0100

    added try and catch with error message

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -7,49 +7,70 @@ const {
 const { v4: uuidv4 } = require("uuid");
 
 const getNotes = (req, res) => {
-  // read from data file
-  const notes = readFromDataFile("db");
-  // send data and return response
-  return res.json(notes);
+  try {
+    // read from data file
+    const notes = readFromDataFile("db");
+
+    // send data and return response
+    return res.json(notes);
+  } catch (error) {
+    console.log(`[ERROR: Server error | ${error.message}]`);
+
+    return res.status(500).json({ suscess: false, error: "Server error" });
+  }
 };
 
-function deleteNote(req, res) {
-  // get id for the note
-  const { id } = req.params;
+const deleteNote = (req, res) => {
+  try {
+    // get id for the note
+    const { id } = req.params;
 
-  // get all data from file
-  const notes = readFromDataFile("db");
+    // get all data from file
+    const notes = readFromDataFile("db");
 
-  // remove note from file
-  const filteredDataFile = notes.filter((item) => {
-    item.id !== id;
-  });
+    // remove note from file
+    const filteredDataFile = notes.filter((item) => {
+      return item.id !== id;
+    });
 
-  // write into data file
-  writeToDataFile("db", filteredDataFile);
+    // write into data file
+    writeToDataFile("db", filteredDataFile);
 
-  // send response
-  res.json({
-    message: "Note has been successfully delated!",
-  });
-}
+    // send response
+    return res.json({
+      message: "Note has been successfully delated!",
+    });
+  } catch (error) {
+    console.log(`[ERROR: Server error | ${error.message}]`);
+
+    return res.status(500).json({ suscess: false, error: "server error" });
+  }
+};
 
 const createNote = (req, res) => {
-  //get data from db
-  const notes = readFromDataFile("db");
+  try {
+    //get data from db
+    const notes = readFromDataFile("db");
 
-  //get data from the user
-  const { title, text } = req.body;
+    //get data from the user
+    const { title, text } = req.body;
 
-  //generate unique id
-  const id = uuidv4();
+    //generate unique id
+    const id = uuidv4();
 
-  //write note into data file
-  const createANote = { id, title, text };
-  writeToDataFile("db", [newNote, ...notes]);
+    //write the note into a data file
+    const createANote = { id, title, text };
+    writeDataToFile("db", [createANote, ...notes]);
 
-  //send file as response - is it needed?
-  res.json(createANote);
+    //read from data file
+    const updatedNotes = readFromDataFile("db");
+
+    //send file as a response
+    return res.json(createANote);
+  } catch (error) {
+    console.log("[ERROR] : Server error");
+    return res.status(500).json({ message: "server error" });
+  }
 };
 
 module.exports = { getNotes, deleteNote, createNote };
