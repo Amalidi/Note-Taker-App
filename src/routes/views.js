db9b5600fdb8f0dcb61e1a8723feb4042473ef9d
Amalidi <A.idi12@outlook.com>
2022-06-22 19:45:05 +0100

    added the data file

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { renderNotePage, renderHomePage } = require("../controllers/view");
+const { renderNotePage, renderHomePage } = require("../controllers/views");
 
 const router = Router();
 
